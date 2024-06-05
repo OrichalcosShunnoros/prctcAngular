9d2a397180ad5f2b371608c512dddb32cad914cb
OrichalcosShunnoros <c3146092091@gmail.com>
2024-06-04 20:26:47 -0500

    continue work on create

--- src/app/components/card-product/card-product.component.ts
+++ src/app/components/card-product/card-product.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card-product',
-  templateUrl: './card-product.component.html',
-  styleUrl: './card-product.component.scss'
+    selector: 'app-card-product',
+    templateUrl: './card-product.component.html',
+    styleUrl: './card-product.component.scss'
 })
-export class CardProductComponent {
+export class CardProductComponent implements OnInit {
+    product = {
+        name: 'Bike',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }
 
+    constructor(){}
+    
+    ngOnInit(){
+        
+    }
 }
